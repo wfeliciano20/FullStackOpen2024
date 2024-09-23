e31b43612d38fae20e899aec57a461c6b4cb8b1a
wfeliciano20 <william.feliciano2@gmail.com>
2024-09-22 23:42:43 -0400

    bug fix on Content,ts

--- Part1/courseInfo/src/Content.tsx
+++ Part1/courseInfo/src/Content.tsx
@@ -10,9 +10,9 @@ interface props {
 const Content = ({ parts }: props) => {
   return (
     <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
       <Part name={parts[1].name} exercises={parts[1].exercises} />
       <Part name={parts[2].name} exercises={parts[2].exercises} />
-      <Part name={parts[3].name} exercises={parts[3].exercises} />
     </div>
   );
 };
