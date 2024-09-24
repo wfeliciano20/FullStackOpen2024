65abc09e2bb742423e0b4f5c4c7a9eb03c06aaa5
wfeliciano20 <william.feliciano2@gmail.com>
2024-09-24 03:15:04 -0400

    anecdotes step3

---
+++ Part1/anecdotes/src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  const [votes, setVotes] = useState<{ [key: number]: number }>({
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0
+  })
+  const [selected, setSelected] = useState(0)
+
+  const maxVoteAnecdote = Object.keys(votes).reduce((a, b) => votes[Number(a)] > votes[Number(b)] ? a : b);
+
+  return (
+    <div>
+      <p>{anecdotes[selected]} </p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={() => setVotes(prev => ({ ...prev, [selected]: prev[selected] + 1 }))}>vote</button>
+      <button onClick={()=> setSelected(Math.floor(Math.random()*anecdotes.length))}>next anecdote</button>
+      <p>Anecdote with most votes: {anecdotes[Number(maxVoteAnecdote)]} with {votes[Number(maxVoteAnecdote)]} votes</p>
+    </div>
+  )
+}
+
+export default App
